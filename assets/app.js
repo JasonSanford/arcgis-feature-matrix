fc5920d3b5e348b9c5e1290ed7df53a3b4d75664
Jason Sanford <jasonsanford@gmail.com>
2015-03-23 15:43:18 -0400

    Make geojson work.

--- assets/app.js
+++ assets/app.js
@@ -8,4 +8,49 @@
     minZoom: 2,
     maxZoom: 6
   }).addTo(map);
+
+  var $buttons = $('.buttons');
+  $buttons.on('click', '.button', function (event) {
+    var $target = $(event.target);
+    var geometry = features[$target.data('name')];
+
+    geojsonLayer.clearLayers();
+
+    if ($target.hasClass('active')) {
+      $target.removeClass('active');
+      return;
+    }
+
+    $target.siblings('button').removeClass('active');
+    $target.addClass('active');
+    geojsonLayer.addData(geometry);
+    map.fitBounds(geojsonLayer.getBounds());
+  });
+
+  var features = {};
+  var geojsonLayer = new L.GeoJSON(null, {
+    style: function () {
+      return {
+        fillOpacity: 0,
+        weight: 2,
+        color: '#333'
+      };
+    }
+  });
+  map.addLayer(geojsonLayer);
+
+  $.ajax({
+    url: 'features.geojson',
+    dataType: 'json',
+    success: function (data) {
+      data.features.forEach(function (feature) {
+        var name = feature.properties.name;
+        features[name] = feature.geometry;
+        $buttons.append('<button data-name="' + name + '" class="button">' + name + '</button>');
+      });
+    },
+    error: function () {
+      console.log('Error when getting features.geojson')
+    }
+  });
 }());
